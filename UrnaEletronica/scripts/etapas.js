fed0a2a0e4f6a0158460f5e19acc6c200467525d
Dego Chiarelotto <diegocamiloaps@gmail.com>
2022-06-21 21:38:59 -0300

    Criação do projeto Urna Eletronica
    Foi criado nessa etapa o Layout, CSS e lista com os vereadores.

---
+++ UrnaEletronica/scripts/etapas.js
@@ -0,0 +1,24 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 10001,
+                nome: "Vereador 01",
+                partido: "PARTIDO-A",
+                fotos:[{
+                    ulr:'vereador01.png', legenda:'Vereador'
+                }]
+            },
+            {
+                numero: 10002,
+                nome: "Vereador 02",
+                partido: "PARTIDO-B",
+                fotos:[{
+                    ulr:'vereador02.png', legenda:'Vereador'
+                }]
+            }
+        ]
+    }
+]
