9fecfcd24de74c33011d1bccc0cd784d0eb6c263
Dego Chiarelotto <diegocamiloaps@gmail.com>
2022-06-23 21:50:21 -0300

    Parte02-Urna
    Implementado verificação de candidato no Array, apresentou um falha em análise

--- UrnaEletronica/scripts/etapas.js
+++ UrnaEletronica/scripts/etapas.js
@@ -4,22 +4,20 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 10001,
-                nome: "Vereador 01",
+                numero: '10001',
+                nome: "Vereador01",
                 partido: "PARTIDO-A",
-                fotos:[{
-                    ulr:'vereador01.png', legenda:'Vereador'
-                },
+                fotos:[
+                {url: 'vereador01.png', legenda: 'Vereador'}
             ]
             },
             {
-                numero: 10002,
-                nome: "Vereador 02",
+                numero: "10002",
+                nome: "Vereador02",
                 partido: "PARTIDO-B",
-                fotos:[{
-                    ulr:'vereador02.png', legenda:'Vereador'
-                },
-            ]
+                fotos:[
+                    {url: 'vereador02.png', legenda: 'Vereador'}
+                ]
             },
         ]
     },
--- UrnaEletronica/scripts/script.js
+++ UrnaEletronica/scripts/script.js
@@ -4,24 +4,24 @@ let cargo = document.querySelector('.d-1-2 span');
 let descGeral = document.querySelector('.d-1-4');
 let avisoVoto = document.querySelector('.d-2');
 let lateral = document.querySelector('.d1-right');
-let dispNumeros = document.querySelector('.d-1-3');
+let numeros = document.querySelector('.d-1-3');
 
 
 //Controle de Ambiente
 let etapaAtual = 0;
 let numero = '';
 
-function comecarEtapa(){
+function comecarEtapa() {
     let etapa = etapas[etapaAtual];
     let numeroHtml = '';
 
 
     numeroHtml = '';
-    for(let i=0;i<etapa.numeros;i++){
-        if(i === 0){
-            numeroHtml +=  '<div class="numeros piscar"></div>';
-        }else {
-            numeroHtml +=  '<div class="numeros"></div>';
+    for (let i = 0; i < etapa.numeros; i++) {
+        if (i === 0) {
+            numeroHtml += '<div class="numeros piscar"></div>';
+        } else {
+            numeroHtml += '<div class="numeros"></div>';
         }
     }
 
@@ -30,39 +30,76 @@ function comecarEtapa(){
     descGeral.innerHTML = '';
     avisoVoto.style.display = 'none';
     lateral.innerHTML = '';
-    dispNumeros.innerHTML = numeroHtml;
+    numeros.innerHTML = numeroHtml;
 }
 
 //Sera execultada em uma ação de digitação
-function atualizaInterace(n){
-    
-}
+function atualizaInterace() {
+    let etapa = etapas[etapaAtual];
+//verificando e o numero digitado for igual a lista do array 
+    let candidato = etapa.candidatos.filter((item) => {
+        if (item.numero === numero) {
+            return true;
+        } else {
+            return false
+        };
+    });
+    if(candidato.length > 0){
+        canditado = candidato[0];//Achou o candidato
+        seuVotoPara.style.display = 'block';
+        avisoVoto.style.display = 'block';
+        descGeral.innerHTML = `Nome:${candidato.nome}`;
+        
+        console.log(candidato);
+        
+    /*       
+ //Montando array de fotos
+    let fotosHTML = '';
+        for(let i in candidato.fotos) {
+            fotosHTML += `<div class="d-1-image">
+            <img src="img/Prefeito01.png" alt="" />
+            ${candidato.fotos[i].legenda}
+        </div>`*/
+        };
+   // lateral.innerHTML = fotosHTML;
+};
+
+
 
 
 //Teclado Numérico
-function clicou(n){
-    let elNumero = document.querySelector('.numeros.pisca');
-    if(elNumero !== null){
+function clicou(n) {
+    let elNumero = document.querySelector('.numeros.piscar');
+    if (elNumero !== null) {
         elNumero.innerHTML = n;
         numero = `${numero}${n}`;
+        //Removendo o piscar apos digitar o número
+        elNumero.classList.remove('piscar');
+        //verificando se cheguei no ultimo elemento        
+        if (elNumero.nextElementSibling !== null) {
+            //Adicionando o piscar para o proximo elemento da sequencia
+            elNumero.nextElementSibling.classList.add('piscar');
+        } else {
+            atualizaInterace();
+        }
+
     }
-       console.log(n)
 }
 
 
 
 //Btn branco
-function branco(){
-   alert('Clicou em BRANCO!')
+function branco() {
+    alert('Clicou em BRANCO!')
 }
 
 //Btn corrige
-function corrige(){
+function corrige() {
     alert('Clicou em CORRIGE!')
 }
 
 //Btn confirma
-function confirma(){
+function confirma() {
     alert('Clicou em CONFIRMA!')
 }
 
